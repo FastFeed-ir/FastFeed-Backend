128464d23b5f12b3addb3d01b5374e9ffc5df2c4
Pouya_Moradi <pouyaamoradii13800@gmail.com>
2023-04-06 02:25:28 +0430

    Fix Collection for admin panel

--- collection/apps.py
+++ collection/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class CollectionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'collection'
+    verbose_name = "دسته بندی"
--- collection/models.py
+++ collection/models.py
@@ -4,9 +4,9 @@ from store.models import Store
 
 
 class Collection(models.Model):
-    title = models.CharField(max_length=32)
-    store = models.ForeignKey(Store, on_delete=models.CASCADE, related_name='collections')
-    is_featured = models.BooleanField(default=False, null=True)
+    title = models.CharField(max_length=32, verbose_name="نام")
+    store = models.ForeignKey(Store, on_delete=models.CASCADE, related_name='collections', verbose_name="فروشگاه")
+    is_featured = models.BooleanField(default=False, null=True, verbose_name="پیشنهادی(اختیاری)")
 
     created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     updated_at = models.DateTimeField(editable=False, null=True, blank=True)
