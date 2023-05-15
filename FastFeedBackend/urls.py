509d95bff6dbe7dc0a6581c7e816c31ecb80943f
Erfan Nasri <erfannasri2@gmail.com>
2023-05-15 15:55:56 +0330

    Add Order Products List APIs

--- FastFeedBackend/urls.py
+++ FastFeedBackend/urls.py
@@ -1,18 +1,16 @@
-from django.conf import settings
-from django.conf.urls.static import static
+# from azbankgateways.urls import az_bank_gateways_urls
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
-from azbankgateways.urls import az_bank_gateways_urls
 
 from comment import views as comment
 from menu import views as menu
 from order import views as order
 from owner import views as owner
+# from payments.views import go_to_gateway_view, callback_gateway_view
 from store import views as store
 from subs import views as subscription
-from payments.views import go_to_gateway_view, callback_gateway_view
 
 router = DefaultRouter()
 router.register('stores', store.StoreViewSet)
@@ -30,7 +28,11 @@ urlpatterns = [
                   path('api-token-auth/', views.obtain_auth_token),
                   path('api-auth/', include('rest_framework.urls')),
                   path('stores/<int:store_id>/tables/<int:table_number>/last-order/', order.LastOrderView.as_view()),
-                  path('bankgateways/', az_bank_gateways_urls()),
-                  path('go-to-gateway/', go_to_gateway_view),
-                  path('callback-gateway/', callback_gateway_view),
+                  path('orders/<int:order_id>/productsID/', order.OrderProductIdListAPIView.as_view(),
+                       name='order-product_id-list'),
+                  path('orders/<int:order_id>/productsName/', order.OrderProductNameListAPIView.as_view(),
+                       name='order-product_name-list'),
+                  # path('bankgateways/', az_bank_gateways_urls()),
+                  # path('go-to-gateway/', go_to_gateway_view),
+                  # path('callback-gateway/', callback_gateway_view),
               ] + router.urls
--- order/views.py
+++ order/views.py
@@ -1,11 +1,12 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 
+from menu.models import Product
 from order.models import Order, OrderItem
 from order.serializers import OrderSerializer, OrderItemSerializer
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.viewsets import ModelViewSet
 
 
 class OrderViewSet(ModelViewSet):
@@ -18,6 +19,7 @@ class OrderViewSet(ModelViewSet):
 
 class LastOrderView(APIView):
     permission_classes = [IsAuthenticated]
+
     def get(self, request, store_id, table_number):
         orders = Order.objects.filter(store_id=store_id, table_number=table_number).order_by('-created_at')
         if orders:
@@ -26,6 +28,31 @@ class LastOrderView(APIView):
             return Response(serializer.data)
         else:
             return Response({'message': 'No orders found for this table.'}, status=404)
+
+
+class OrderProductIdListAPIView(APIView):
+    def get(self, request, order_id):
+        order_items = OrderItem.objects.filter(order_id=order_id)
+        if order_items:
+            product_ids = [item.product_id for item in order_items]
+            return Response({'product_ids': product_ids})
+        else:
+            return Response({'message': 'No orderItem found for this order.'}, status=404)
+
+
+class OrderProductNameListAPIView(APIView):
+    def get(self, request, order_id):
+        try:
+            order = Order.objects.get(id=order_id)
+        except Order.DoesNotExist:
+            return Response({'message': 'Order not found.'}, status=404)
+
+        order_items = OrderItem.objects.filter(order=order)
+        product_ids = [order_item.product_id for order_item in order_items]
+        products = Product.objects.filter(id__in=product_ids)
+        product_names = [product.title for product in products]
+
+        return Response({'product_names': product_names})
 
 
 class OrderItemViewSet(ModelViewSet):
