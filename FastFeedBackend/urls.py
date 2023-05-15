0039d0cb81b987cbae4f528cb4710d58e46ee06e
Erfan Nasri <erfannasri2@gmail.com>
2023-05-15 20:43:05 +0330

    Fix url router

--- FastFeedBackend/urls.py
+++ FastFeedBackend/urls.py
@@ -1,4 +1,3 @@
-# from azbankgateways.urls import az_bank_gateways_urls
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken import views
@@ -8,7 +7,6 @@ from comment import views as comment
 from menu import views as menu
 from order import views as order
 from owner import views as owner
-# from payments.views import go_to_gateway_view, callback_gateway_view
 from store import views as store
 from subs import views as subscription
 
@@ -22,18 +20,14 @@ router.register('owners', owner.BusinessOwnerViewSet)
 router.register('orders', order.OrderViewSet)
 router.register('order_items', order.OrderItemViewSet)
 router.register('ratings', comment.RatingViewSet)
-router.register(r'order-comments', comment.OrderCommentViewSet, basename='order-comments')
 
 urlpatterns = [
-                  path('admin/', admin.site.urls),
-                  path('api-token-auth/', views.obtain_auth_token),
-                  path('api-auth/', include('rest_framework.urls')),
-                  path('stores/<int:store_id>/tables/<int:table_number>/last-order/', order.LastOrderView.as_view()),
-                  path('orders/<int:order_id>/productsID/', order.OrderProductIdListAPIView.as_view(),
-                       name='order-product_id-list'),
-                  path('orders/<int:order_id>/productsName/', order.OrderProductNameListAPIView.as_view(),
-                       name='order-product_name-list'),
-                  # path('bankgateways/', az_bank_gateways_urls()),
-                  # path('go-to-gateway/', go_to_gateway_view),
-                  # path('callback-gateway/', callback_gateway_view),
-              ] + router.urls
+    path('admin/', admin.site.urls),
+    path('api-token-auth/', views.obtain_auth_token),
+    path('api-auth/', include('rest_framework.urls')),
+    path('stores/<int:store_id>/tables/<int:table_number>/last-order/', order.LastOrderView.as_view()),
+    path('orders/<int:order_id>/productsID/', order.OrderProductIdListAPIView.as_view(), name='order-product_id-list'),
+    path('orders/<int:order_id>/productsName/', order.OrderProductNameListAPIView.as_view(),
+         name='order-product_name-list'),
+    path('order-comments/', comment.OrderCommentViewSet.as_view({'get': 'list'}), name='order-comment-list'),
+] + router.urls
